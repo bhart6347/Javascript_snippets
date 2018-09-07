5c6a8806bdca0d6a6d8bec28eea8070a969cb4f3
bhart6347 <brandenahart@yahoo.com>
2018-09-07 18:00:20 -0400

    Added extra commentary to the modification of a const variable

--- let_const_example2.js
+++ let_const_example2.js
@@ -8,4 +8,5 @@ console.log(x);
 x.push(4);
 console.log(x)
 //const values can be mutated by adding or removing a value through any other means than a direct
-//assignment, such as pushing a value to an array.
+//assignment, such as pushing a value to an array. As long as you are not trying to re-bind the const
+//variable to a new value, but simply modifying an existing value, you're good.
